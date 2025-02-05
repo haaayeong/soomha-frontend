2e9e66837d82a6798de4a8f1a04777806b100e1f
yeong <chlgkdud111@gmail.com>
2025-02-05 10:13:28 +0900

    초기 커밋

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/api/test")
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error(err));
+  }, []);
+
+  return (
+    <>
+      <div>
+        <h1>React + Flask 연결</h1>
+        <p>{message}</p>
+      </div>
+    </>
+  )
+}
+
+export default App
