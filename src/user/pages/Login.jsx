c615f622dfe210dfb6a7c495d11cd86dd3723182
yeong <chlgkdud111@gmail.com>
2025-02-12 11:45:38 +0900

    Update: 로그인페이지에 header, footer 추가

--- src/user/pages/Login.jsx
+++ src/user/pages/Login.jsx
@@ -1,24 +1,31 @@
 import { useNavigate } from "react-router-dom";
 import "../style/Login.css";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
 
 function Login() {
   const navigate = useNavigate();
 
-  return(
-    <div className="login">
-      <div className="content-wrap">
-        <h3>로그인</h3>
-        <input type="text" name="username" placeholder="아이디를 입력하세요"/>
-        <input type="text" name="password" placeholder="비밀번호를 입력하세요"/>
-        <button>로그인</button>
-        
-        <div className="content-footer">
-          <p>아직 회원이 아니신가요?</p>
-          <p onClick={() => navigate('/signup')}>회원가입</p>
-        </div>
-      </div>
+  return (
+    <>
+      <main>
+        <Header />
+        <div className="login">
+          <div className="content-wrap">
+            <h3>로그인</h3>
+            <input type="text" name="username" placeholder="아이디를 입력하세요" />
+            <input type="text" name="password" placeholder="비밀번호를 입력하세요" />
+            <button>로그인</button>
 
-    </div>
+            <div className="content-footer">
+              <p>아직 회원이 아니신가요?</p>
+              <p onClick={() => navigate('/signup')}>회원가입</p>
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </main>
+    </>
   )
 }
 
