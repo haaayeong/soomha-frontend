5d72e097ea42df0b1e06a3de64335793e4e0f018
yeong <chlgkdud111@gmail.com>
2025-02-12 11:50:11 +0900

    Update: 회원가입페이지에 header, footer 추가

--- src/user/pages/Login.jsx
+++ src/user/pages/Login.jsx
@@ -7,25 +7,23 @@ function Login() {
   const navigate = useNavigate();
 
   return (
-    <>
-      <main>
-        <Header />
-        <div className="login">
-          <div className="content-wrap">
-            <h3>로그인</h3>
-            <input type="text" name="username" placeholder="아이디를 입력하세요" />
-            <input type="text" name="password" placeholder="비밀번호를 입력하세요" />
-            <button>로그인</button>
+    <main>
+      <Header />
+      <div className="login">
+        <div className="content-wrap">
+          <h3>로그인</h3>
+          <input type="text" name="username" placeholder="아이디를 입력하세요" />
+          <input type="text" name="password" placeholder="비밀번호를 입력하세요" />
+          <button>로그인</button>
 
-            <div className="content-footer">
-              <p>아직 회원이 아니신가요?</p>
-              <p onClick={() => navigate('/signup')}>회원가입</p>
-            </div>
+          <div className="content-footer">
+            <p>아직 회원이 아니신가요?</p>
+            <p onClick={() => navigate('/signup')}>회원가입</p>
           </div>
         </div>
-        <Footer />
-      </main>
-    </>
+      </div>
+      <Footer />
+    </main>
   )
 }
 
--- src/user/pages/Signup.jsx
+++ src/user/pages/Signup.jsx
@@ -4,6 +4,8 @@ import EmailInput from "../components/EmailInput";
 import RegionSelect from "../components/RegionSelect";
 import '../style/Signup.css'
 import { useNavigate } from "react-router-dom";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 
 function Signup() {
   const [username, setUsername] = useState("");
@@ -91,99 +93,103 @@ function Signup() {
   };
 
   return(
-    <div className="Signup">
-      <div className="content-wrap">
-        <form onSubmit={handleSubmit}>
-          <h3>회원가입</h3>
-          <div id="id">
-            <p>아이디</p>
-            <div className="input-container">
-            <input
-            type="text" 
-            name="username"
-            placeholder="아이디 입력(4~20자)"
-            value={username}
-            onChange={handleChange(setUsername, isValidUsername, setUsernameError)}
-            required
-            />
-            <button id="id-check">중복 확인</button>
+    <main>
+      <Header />
+      <div className="Signup">
+        <div className="content-wrap">
+          <form onSubmit={handleSubmit}>
+            <h3>회원가입</h3>
+            <div id="id">
+              <p>아이디</p>
+              <div className="input-container">
+              <input
+              type="text" 
+              name="username"
+              placeholder="아이디 입력(4~20자)"
+              value={username}
+              onChange={handleChange(setUsername, isValidUsername, setUsernameError)}
+              required
+              />
+              <button id="id-check">중복 확인</button>
+              </div>
+              {usernameError && <p className="error">{usernameError}</p>}
             </div>
-            {usernameError && <p className="error">{usernameError}</p>}
-          </div>
 
-          <div id="password">
-            <p>비밀번호</p>
-            <input
-            type="password"
-            name="password"
-            placeholder="비밀번호 입력(소문자, 숫자, 특수문자 포함 4~20자)"
-            value={password}
-            onChange={handleChange(setPassword, isValidPassword, setPasswordError)}
-            required
-            />
-            {passwordError && <p className="error">{passwordError}</p>}
-          </div>
+            <div id="password">
+              <p>비밀번호</p>
+              <input
+              type="password"
+              name="password"
+              placeholder="비밀번호 입력(소문자, 숫자, 특수문자 포함 4~20자)"
+              value={password}
+              onChange={handleChange(setPassword, isValidPassword, setPasswordError)}
+              required
+              />
+              {passwordError && <p className="error">{passwordError}</p>}
+            </div>
 
-          <div id="confirm-password">
-            <p>비밀번호 확인</p>
-            <input
-            type="password" 
-            name="confirmPassword" 
-            placeholder="비밀번호 재입력"
-            value={confirmPassword}
-            onChange={handleChange(setConfirmPassword, isValidPasswordMatch, setConfirmPasswordError)}
-            required
-            />
-            {confirmPasswordError && <p className="error">{confirmPasswordError}</p>}
-          </div>
+            <div id="confirm-password">
+              <p>비밀번호 확인</p>
+              <input
+              type="password" 
+              name="confirmPassword" 
+              placeholder="비밀번호 재입력"
+              value={confirmPassword}
+              onChange={handleChange(setConfirmPassword, isValidPasswordMatch, setConfirmPasswordError)}
+              required
+              />
+              {confirmPasswordError && <p className="error">{confirmPasswordError}</p>}
+            </div>
 
-          <div id="nickname">
-            <p>닉네임</p>
-            <input
-            type="text"
-            name="nickname"
-            placeholder="닉네임 입력"
-            value={nickname}
-            onChange={handleChange(setNickname, isValidNickname, setNicknameError)}
-            required
-            />
-          </div>
+            <div id="nickname">
+              <p>닉네임</p>
+              <input
+              type="text"
+              name="nickname"
+              placeholder="닉네임 입력"
+              value={nickname}
+              onChange={handleChange(setNickname, isValidNickname, setNicknameError)}
+              required
+              />
+            </div>
 
-          <EmailInput setEmail={setEmail} setEmailcode={setEmailCode} />
+            <EmailInput setEmail={setEmail} setEmailcode={setEmailCode} />
 
-          <div id="role">
-            <p>가입 유형</p>
-            <select value={role} onChange={handleRolechange}>
-              <option value="">가입 유형 선택</option>
-              <option value="child">어린이</option>
-              <option value="parent">학부모</option>
-              <option value="teacher">선생님</option>
-            </select>
-          </div>
+            <div id="role">
+              <p>가입 유형</p>
+              <select value={role} onChange={handleRolechange}>
+                <option value="">가입 유형 선택</option>
+                <option value="child">어린이</option>
+                <option value="parent">학부모</option>
+                <option value="teacher">선생님</option>
+              </select>
+            </div>
 
-          {/* role이 'teacher'일 때만 소속 유치원 입력창 표시 */}
-          {role === "teacher" && (
-            <div id="kindergarten">
-            <p>소속 유치원</p>
-            <input 
-            type="text"
-            name="kindergarten"
-            placeholder="소속 유치원 이름 입력"
-            value={kindergarten}
-            onChange={(e) => setKindergarten(e.target.value)}
-             />
-          </div>
-          )} 
+            {/* role이 'teacher'일 때만 소속 유치원 입력창 표시 */}
+            {role === "teacher" && (
+              <div id="kindergarten">
+              <p>소속 유치원</p>
+              <input 
+              type="text"
+              name="kindergarten"
+              placeholder="소속 유치원 이름 입력"
+              value={kindergarten}
+              onChange={(e) => setKindergarten(e.target.value)}
+              />
+            </div>
+            )} 
 
-          <RegionSelect setArea={setArea}/>
+            <RegionSelect setArea={setArea}/>
 
-          <button type="submit" disabled={loading}>
-            {loading ? "회원가입 중..." : "회원가입"}
-          </button>
-        </form>
-        {error && <p>{error}</p>}
+            <button type="submit" disabled={loading}>
+              {loading ? "회원가입 중..." : "회원가입"}
+            </button>
+          </form>
+          {error && <p>{error}</p>}
+        </div>
       </div>
-    </div>
+      <Footer />
+    </main>
   )
 
 }
