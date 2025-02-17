d97cd32abc2e40de5ffdc97e896893f1659c7c34
yeong <chlgkdud111@gmail.com>
2025-02-17 11:00:54 +0900

    Add: 로그인 상태 관리 기능 구현

---
+++ src/user/util/useAuth.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function useAuth() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      axios
+        .get("http://localhost:5000/auth/user", {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        .then((response) => setUser(response.data.user))
+        .catch(() => setUser(null));
+    }
+  }, []);
+
+  return user;
+
+}
+
+export default useAuth;
