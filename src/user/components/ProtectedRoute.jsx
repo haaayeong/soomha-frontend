077326fdaf9d3158dde29e24fc45f99e858f6dd7
yeong <chlgkdud111@gmail.com>
2025-02-18 14:25:31 +0900

    Upgrade : 로그인이 필요한 페이지에 로그인 안 한 상태로 들어가면 로그인 페이지로 이동하는데 그 전에 alert 창으로 문구를 띄워줄 수 있게 해줌.

--- src/user/components/ProtectedRoute.jsx
+++ src/user/components/ProtectedRoute.jsx
@@ -1,9 +1,20 @@
+import { useEffect, useRef, useState } from "react";
 import { Navigate } from "react-router-dom";
 
 function ProtectedRoute ({ children }) {
-  const token = localStorage.getItem("token");
+  const [token] = useState(localStorage.getItem("token"));
+  const alertShown = useRef(false);
 
-  return token ? children : <Navigate to="/login" />;
+  if (!token && !alertShown.current) {
+    alert("로그인이 필요한 페이지입니다.");
+    alertShown.current = true;
+  }
+
+  if (!token) {
+    return <Navigate to="/login" />
+  }
+
+  return children;
 };
 
 export default ProtectedRoute;
